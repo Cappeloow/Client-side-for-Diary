6f8b82ea78a15f1d534cfb7c40f44c8f74f4dc36
cappeloow <cappe1996@hotmail.com>
2023-03-03 11:09:44 +0100

    now we can get all public posts and post aswell!

--- dist/mystory.js
+++ dist/mystory.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 const sideBar = document.querySelector(".sidebar");
 const startingPhrase = () => {
     let user = localStorage.getItem("user");
@@ -7,11 +16,66 @@ const startingPhrase = () => {
         const paragraphWelcomeUser = document.createElement("p");
         parsedUser.name = parsedUser.username.split('@')[0];
         paragraphWelcomeUser.innerText = `${parsedUser.name}`;
-        console.log(parsedUser);
         sideBar.appendChild(paragraphWelcomeUser);
+        return parsedUser;
     }
     else {
         window.location.href = "index.html";
+        return undefined;
     }
 };
-startingPhrase();
+const LoggedUser = startingPhrase();
+console.log(LoggedUser);
+const contentInput = document.querySelector(".contentInput");
+const titleInput = document.querySelector(".titleInput");
+const submitPost = document.querySelector(".submitPost");
+const createPostsToDatabase = () => __awaiter(void 0, void 0, void 0, function* () {
+    if (!LoggedUser) {
+        throw new Error('User is not logged in');
+    }
+    try {
+        const response = yield fetch("http://localhost:3000/api/post/send", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                user: LoggedUser.name,
+                title: titleInput.value,
+                content: contentInput.value
+            })
+        });
+        const data = yield response.json();
+        titleInput.value = "";
+        contentInput.value = "";
+        console.log(data);
+        if (!response.ok) {
+            throw new Error('Request failed');
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+});
+submitPost.addEventListener("click", createPostsToDatabase);
+const allPosts = document.querySelector(".allPosts");
+const displayPost = (post) => {
+    const postTemplate = `
+  <div class="post">
+    <h2>${post.user}</h2>
+    <h3>${post.title}</h3>
+    <p>${post.content}</p>
+  </div>
+  `;
+    allPosts.insertAdjacentHTML('beforeend', postTemplate);
+};
+/***************FETCH ALL USER POSTS*********************/
+console.log(allPosts);
+const getAllPosts = () => __awaiter(void 0, void 0, void 0, function* () {
+    const response = yield fetch("http://localhost:3000/api/post/public");
+    const data = yield response.json();
+    data.forEach((post) => {
+        displayPost(post);
+    });
+});
+getAllPosts();
--- mystory.ts
+++ mystory.ts
@@ -1,19 +1,98 @@
 const sideBar = document.querySelector(".sidebar") as HTMLDivElement;
 
-const startingPhrase = () => {
-    let user = localStorage.getItem("user");
+type User ={
+  _id:string,
+  username:string,
+  isAdmin:string,
+  name:string
+}
+
+const startingPhrase = ():User |undefined=> {
+  let user = localStorage.getItem("user");
     if (user !== null) {
       let parsedUser = JSON.parse(user);
       const paragraphWelcomeUser = document.createElement("p") as HTMLParagraphElement;
       parsedUser.name = parsedUser.username.split('@')[0];
       paragraphWelcomeUser.innerText = `${parsedUser.name}`;
-      console.log(parsedUser);
       sideBar.appendChild(paragraphWelcomeUser);
+      return parsedUser;
     } else {
       window.location.href = "index.html";
+      return undefined;
     }
 }
-startingPhrase();
+const LoggedUser:User | undefined= startingPhrase();
+console.log(LoggedUser);
+
+const contentInput = document.querySelector(".contentInput") as HTMLInputElement;
+const titleInput = document.querySelector(".titleInput") as HTMLTextAreaElement;
+const submitPost = document.querySelector(".submitPost") as HTMLButtonElement;
 
 
+const createPostsToDatabase = async () => {
+  if (!LoggedUser) {
+    throw new Error('User is not logged in');
+  }
+  try {
+    const response = await fetch("http://localhost:3000/api/post/send", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        user:LoggedUser.name,
+        title: titleInput.value,
+        content: contentInput.value
+      })    
+    });
+  
+    const data = await response.json();
+    titleInput.value="";
+    contentInput.value="";
+    console.log(data);
+    if (!response.ok) {
+      throw new Error('Request failed');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+ 
+}
 
+
+submitPost.addEventListener("click",createPostsToDatabase);
+
+type Post = {
+  content:string,
+  user:string,
+  title:string
+}
+const allPosts = document.querySelector(".allPosts") as HTMLDivElement;
+
+
+const displayPost = (post:Post) => {
+  const postTemplate = `
+  <div class="post">
+    <h2>${post.user}</h2>
+    <h3>${post.title}</h3>
+    <p>${post.content}</p>
+  </div>
+  `
+  allPosts.insertAdjacentHTML('beforeend',postTemplate);
+}
+
+/***************FETCH ALL USER POSTS*********************/
+console.log(allPosts);
+
+const getAllPosts = async () => {
+const response = await fetch("http://localhost:3000/api/post/public");
+const data = await response.json();
+
+data.forEach((post:Post) => {
+  displayPost(post);
+});
+
+};
+
+
+getAllPosts();
